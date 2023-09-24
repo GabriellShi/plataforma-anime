793bb1a464c4b9bf1640f8ae9ffef4d2991886ff
GabriellShi <gabrieloliveirasantos196@gmail.com>
2023-09-24 21:56:49 +0100

    atualização dos botões de paginas no final das paginas

--- src/controllers/PaginasController.js
+++ src/controllers/PaginasController.js
@@ -16,35 +16,71 @@ const paginasController = {
 
   listaAnimeUsuario: async (req, res) => {
     try {
-      // Busque todas as notícias do banco de dados
+      const page = req.query.page || 1; // Página atual, padrão é 1
+      const perPage = 20; // Número de animes por página
+  
+      // Calcule o índice de início e fim com base na página atual
+      const startIndex = (page - 1) * perPage;
+      const endIndex = startIndex + perPage;
+  
+      // Busque todos os animes do banco de dados
       const listaAnimeAdmin = await Animes.findAll({
+        order: [['created_at', 'DESC']]
       });
-
-
-    return res.render("listaAnimeUsuario", {
-      title: "Lista de Anime",
-      listaAnimeAdmin,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).render("error", {
-      title: "Erro",
-      message: "Ocorreu um erro ao carregar a lista de Animes",
-    });
-  }
+  
+      // Filtrar os animes para a página atual
+      const listaAnimeAdminPaginaAtual = listaAnimeAdmin.slice(startIndex, endIndex);
+  
+      // Calcule o número total de animes, o número total de páginas e a página atual
+      const totalAnimes = listaAnimeAdmin.length;
+      const totalPages = Math.ceil(totalAnimes / perPage);
+  
+      return res.render("listaAnimeUsuario", {
+        title: "Lista de Anime",
+        listaAnimeAdmin: listaAnimeAdminPaginaAtual,
+        page, // Página atual
+        totalPages, // Número total de páginas
+        totalAnimes, // Número total de animes
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).render("error", {
+        title: "Erro",
+        message: "Ocorreu um erro ao carregar a lista de Animes",
+      });
+    }
   },
+  
 
   episodiosAdicionados: async (req, res) => {
 
     try {
+
+      const page = req.query.page || 1; // Página atual, padrão é 1
+      const perPage = 20; // Número de filmes por página
+  
+      // Calcule o índice de início e fim com base na página atual
+      const startIndex = (page - 1) * perPage;
+      const endIndex = startIndex + perPage;
+  
       // Busque todas as notícias do banco de dados
       const episodios = await Episodios.findAll({
         order: [['created_at', 'DESC']]
       });
-  
+
+      const episodiosPaginaAtual = episodios.slice(startIndex, endIndex);
+
+        // Calcule o número total de animes, o número total de páginas e a página atual
+        const totalEpisodios = episodios.length;
+        const totalPages = Math.ceil(totalEpisodios / perPage);
+
       return res.render("episodiosAdicionados", {
         title: "Lista de Episodios",
         episodios,
+        episodios: episodiosPaginaAtual,
+        page, // Página atual
+        totalPages, // Número total de páginas
+        totalEpisodios, // Número total de animes
       
       });
     } catch (error) {
@@ -57,28 +93,43 @@ const paginasController = {
   },
 
 
-  
   filmesAdicionados: async (req, res) => {
-
     try {
-      // Busque todas as notícias do banco de dados
+      const page = req.query.page || 1; // Página atual, padrão é 1
+      const perPage = 20; // Número de filmes por página
+  
+      // Calcule o índice de início e fim com base na página atual
+      const startIndex = (page - 1) * perPage;
+      const endIndex = startIndex + perPage;
+  
+      // Busque todos os filmes do banco de dados
       const filmes = await Filmes.findAll({
-        order: [['created_at', 'DESC']]
+        order: [['created_at', 'DESC']],
       });
+  
+      // Filtrar os filmes para a página atual
+      const filmesPaginaAtual = filmes.slice(startIndex, endIndex);
 
+          // Calcule o número total de animes, o número total de páginas e a página atual
+          const totalFilmes = filmes.length;
+          const totalPages = Math.ceil(totalFilmes / perPage);
+  
       return res.render("filmesAdicionados", {
         title: "Filmes Adicionados",
-        filmes,
-      
+        filmes: filmesPaginaAtual,
+        page, // Página atual
+        totalPages, // Número total de páginas
+        totalFilmes, // Número total de animes
       });
     } catch (error) {
       console.error(error);
       return res.status(500).render("error", {
         title: "Erro",
-        message: "Ocorreu um erro ao carregar a Lista Filmes",
+        message: "Ocorreu um erro ao carregar a Lista de Filmes",
       });
     }
   },
+  
 
   calendarioAnimes: async (req, res) => {
     const lancamento = await Lancamento.findAll({
